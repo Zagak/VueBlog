8e550c4e6f62445db6d34874fa35f00107885572
Eduard <costache.edy2001@gmail.com>
2024-01-26 13:48:27 +0200

    again

--- app.vue
+++ app.vue
@@ -7,17 +7,6 @@
 </template>
 
 <script setup>
-import { usePostsStore } from '../store/usePostsStore';
-import * as contentful from 'contentful';
 
-
-const config = useRuntimeConfig()
-const contentfulClient = contentful.createClient({
-  space: config.public.CONTENTFUL_SPACE_ID,
-  accessToken: config.public.CONTENTFUL_ACCES_KEY,
-})
-
-const store = usePostsStore()
-await store.fetchPosts(contentfulClient)
 
 </script>
