95a895fab515c370aad4c6cd690ec623e5cfdae9
Eduard <costache.edy2001@gmail.com>
2024-01-26 13:58:09 +0200

    again 1

--- pages/Home.vue
+++ pages/Home.vue
@@ -50,6 +50,18 @@ const features = [
   { "title": "Stylish Performance Wear", "description": "Merge style with performance, designed not just for function but to complement your individual taste and elevate your fashion-forward, active lifestyle." }
 ]
 
+import { usePostsStore } from '../store/usePostsStore';
+import * as contentful from 'contentful';
+
+
+const config = useRuntimeConfig()
+const contentfulClient = contentful.createClient({
+  space: config.public.CONTENTFUL_SPACE_ID,
+  accessToken: config.public.CONTENTFUL_ACCES_KEY,
+})
+
+const store = usePostsStore()
+await store.fetchPosts(contentfulClient)
 </script>
 
 <style></style>
