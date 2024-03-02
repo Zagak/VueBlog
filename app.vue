917c59104d22c19503ffd55046cd207f04ca6ae5
Eduard <costache.edy2001@gmail.com>
2024-03-02 11:29:30 +0200

    test -> no CMS request on root again

--- app.vue
+++ app.vue
@@ -10,11 +10,11 @@
 import { usePostsStore } from '../store/usePostsStore';
 import * as contentful from 'contentful';
 
-const config = useRuntimeConfig()
-const contentfulClient = contentful.createClient({
-  space: config.public.CONTENTFUL_SPACE_ID,
-  accessToken: config.public.CONTENTFUL_ACCES_KEY,
-})
+// const config = useRuntimeConfig()
+// const contentfulClient = contentful.createClient({
+//   space: config.public.CONTENTFUL_SPACE_ID,
+//   accessToken: config.public.CONTENTFUL_ACCES_KEY,
+// })
 
 // onBeforeMount(async () => {
 //   const store = usePostsStore()
