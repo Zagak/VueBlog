4a73ca8a5788575fe8cb55d1a8ae83e6a383848a
Eduard <costache.edy2001@gmail.com>
2024-03-02 11:37:58 +0200

    test -> contentfull client creation in beforeMount hook

--- app.vue
+++ app.vue
@@ -11,12 +11,14 @@ import { usePostsStore } from '../store/usePostsStore';
 import * as contentful from 'contentful';
 
 const config = useRuntimeConfig()
-const contentfulClient = contentful.createClient({
-  space: config.public.CONTENTFUL_SPACE_ID,
-  accessToken: config.public.CONTENTFUL_ACCES_KEY,
-})
+
 
 onBeforeMount(async () => {
+  const contentfulClient = contentful.createClient({
+    space: config.public.CONTENTFUL_SPACE_ID,
+    accessToken: config.public.CONTENTFUL_ACCES_KEY,
+  })
+
   const store = usePostsStore()
   await store.fetchPosts(contentfulClient)
 })
