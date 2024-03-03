f98a812ad7cdf8b833c50846406803db7d57f431
Eduard <costache.edy2001@gmail.com>
2024-03-03 18:19:55 +0200

    using loading state

--- app.vue
+++ app.vue
@@ -1,25 +1,40 @@
 <template>
-  <div class="bg-primary">
+  <div v-if="isLoading">
+    Loading ...
+  </div>
+  <div v-else class="bg-primary">
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
+<!-- await useAsyncData(
+  'mountains',
+  () => store.fetchPosts(contentfulClient)
+) -->
 
 <script setup>
 import { usePostsStore } from '../store/usePostsStore';
 import * as contentful from 'contentful';
 
 const config = useRuntimeConfig()
+let isLoading = true;
 
-
-onMounted(async () => {
-  const contentfulClient = contentful.createClient({
-    space: config.public.CONTENTFUL_SPACE_ID,
-    accessToken: config.public.CONTENTFUL_ACCES_KEY,
-  })
-
-  const store = usePostsStore()
-  await store.fetchPosts(contentfulClient)
+// onMounted(async () => {
+const contentfulClient = contentful.createClient({
+  space: config.public.CONTENTFUL_SPACE_ID,
+  accessToken: config.public.CONTENTFUL_ACCES_KEY,
 })
+
+const store = usePostsStore()
+
+try {
+  await store.fetchPosts(contentfulClient)
+} catch (err) {
+  console.error('Error fetching data:', error);
+} finally {
+  isLoading = false
+}
+
+// })
 </script>
