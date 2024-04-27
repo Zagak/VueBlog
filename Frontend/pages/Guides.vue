ef88a841298c32ff718576b290914a4918c12f93
Eduard <costache.edy2001@gmail.com>
2024-04-27 16:18:22 +0300

    change

--- Frontend/pages/Guides.vue
+++ Frontend/pages/Guides.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="font-semibold text-6xl text-center py-10">Guides</h1>
-    <ShowPosts category="Guides" class="py-10" />
+    <!-- <ShowPosts category="Guides" class="py-10" /> -->
   </div>
 </template>
 
